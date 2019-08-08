0da81f7faf1d74cc7069a6cee59200ca7d61ff94
Morris Arroyo <arroyomorris28@gmail.com>
2019-08-08 13:31:04 -0700

    added assetBundle

---
+++ Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        //Change BuildTarget to BuildTarget.StandaloneWindows?
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    void Start()
+    {
+        Debug.Log(Path.Combine(Application.dataPath + "/AssetBundles", "paintball"));
+        var myLoadedAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.dataPath + "/AssetBundles", "paintball"));
+        //var myLoadedAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "paintball"));
+        if (myLoadedAssetBundle == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
+        myLoadedAssetBundle.LoadAllAssets();
+        Debug.Log("Loaded Asset");
+        var prefab = myLoadedAssetBundle.LoadAsset<GameObject>("Ammo");
+        GameObject obj = Instantiate(prefab);
+        obj.transform.position = new Vector3(2, 0, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
